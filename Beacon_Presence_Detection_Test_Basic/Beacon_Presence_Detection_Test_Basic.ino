dcc0b465d005edc3e854846a1ed5b4ed87cef19f
Lucas O'Conor <lvoconor@gmail.com>
2014-03-01 02:26:51 -0800

    Fixed Beacon Presence issue with testing whether signal is dead

--- Beacon_Presence_Detection_Test_Basic/Beacon_Presence_Detection_Test_Basic.ino
+++ Beacon_Presence_Detection_Test_Basic/Beacon_Presence_Detection_Test_Basic.ino
@@ -14,6 +14,7 @@
 
 /*---------------- Includes ---------------------------------*/
 #include <Winklelib.h>
+#include <Timers.h>
 /*---------------- Module Defines ---------------------------*/
 #define TAPE_INPUT_PIN 2
 #define BEACON_INPUT_PIN 3
@@ -22,7 +23,7 @@
   
 #define SERVER_BEACON_MICROS 1176
 #define EXCHANGE_BEACON_MICROS 333
-#define NO_SIGNAL_MICROS 2000
+#define NO_SIGNAL_MICROS 10000
 
 
 /*---------------- Module Function Prototypes ---------------*/
@@ -31,19 +32,25 @@ unsigned char TestForKey(void);
 void RespToKey();
 /*---------------- Module Level Variables -------------------*/
 volatile unsigned long previousTime = 0;
-volatile unsigned long period = NO_SIGNAL_MICROS; 
+volatile unsigned long period = NO_SIGNAL_MICROS;
+boolean risingEdgeFlag = false; 
 /*---------------- Arduino Main Functions -------------------*/
 void setup() {
   Serial.begin(9600);
   Serial.println("The Beacon_Presence_Detection_Test program has started!");
   pinMode(BEACON_INPUT_PIN, INPUT);
-  attachInterrupt(1, riseDetected, RISING);  
+  attachInterrupt(1, riseDetected, RISING);
+  TMRArd_InitTimer(0, 2);
 }
 
 void loop() {
-  unsigned long currentTime = micros(); 
-  if(currentTime >= NO_SIGNAL_MICROS){
-    period = NO_SIGNAL_MICROS;
+  if(TMRArd_IsTimerExpired(0)){
+    if(!risingEdgeFlag){
+      period = NO_SIGNAL_MICROS;
+    }
+    risingEdgeFlag = false;
+    TMRArd_InitTimer(0, 2);
+
   }
   if(TestForKey()) RespToKey();
 }
@@ -54,7 +61,7 @@ void riseDetected() {
   unsigned long currentTime = micros(); 
   period = currentTime - previousTime;
   previousTime = currentTime; 
-  
+  risingEdgeFlag = true;
 }  
 
 
@@ -68,16 +75,16 @@ unsigned char TestForKey(void) {
 void RespToKey() {
   unsigned char theKey;
   theKey = Serial.read();
-  unsigned long currentPeriod = period; // make a copy so it doesn't get changed by interrupts
-  if(currentPeriod > SERVER_BEACON_MICROS + 10 || currentPeriod < SERVER_BEACON_MICROS - 10){
+  if(period > SERVER_BEACON_MICROS + 10 || period < SERVER_BEACON_MICROS - 10){
     Serial.println("Server Beacon out of range");
   }
   else{
     Serial.println("Server Beacon in range");
   }
   Serial.print("Period: ");
-  Serial.println(currentPeriod);
+  Serial.println(period);
   Serial.println("--------------------------");
+  
 } 
     
  
