c5bf47102ba8a0652e6c9e3c4411645ffe1b0acc
elizabethmarshman <elizabeth.marshman@gmail.com>
2014-03-03 17:45:20 -0800

    trying to make ramp only push up if both bumpers activated for one second

--- _0303_RampPush/_0303_RampPush.ino
+++ _0303_RampPush/_0303_RampPush.ino
@@ -17,6 +17,8 @@
 #include <Winklelib.h>
 #include <Servo.h>
 
+#define ONE_SEC            1000
+
 
 Servo myservo;
 int SERVO = 10;
@@ -28,8 +30,53 @@ void setup() {
   myservo.attach(10);
 }
 
+//Main Functions
 void loop() {  
-  myservo.write(0);  // <--90 when activated
+    if (TestForBump()) RespToBump();
 }
-      
+
+//Module Functions
+unsigned char TestForBump(void) {
+  static unsigned char LastBumper = 0xF0;
+  //need to check to make sure all these pins (0xF0) are the correct ones
+  unsigned char bumper;
+  unsigned char EventOccurred;
+  
+  bumper = ReadBumpers();
+  EventOccurred = ((bumper != 0xF0) && (bumper != LastBumper));
+  if (EventOccurred) {
+    SET_SHARED_BYTE_TO(bumper);
+    LastBumper = bumper;
+  }
+  return EventOccurred;
+}
+
+void RespToBump(void) {
+    unsigned char bumper;
+
+    bumper = GET_SHARED_BYTE();
+
+    // display which bumper(s) were hit
+    switch (bumper) {
+        case (0xD0):  Serial.println("  Front Right..."); break;
+        case (0xE0):  Serial.println("  Front Left..."); break;
+        case (0x70):  Serial.println("  Back Left..."); break;
+        case (0xB0):  Serial.println("  Back Right..."); break;
+
+        case (0xC0):  Serial.println("  Both Front ...");
+//both bummpers have to be activated for one second before the ramp tilts
+          if {(LFB = 1) && (RFB = 1)
+          (ONE_SEC)};    //not sure how to implement this. here or in WinkleLib?
+          myservo.write(90);  // <--90 is 'on' - ramp high; 0 is 'off' - ramp low
+          break;
+          
+        case (0x30):  Serial.println("  Both Back..."); break;
+        case (0x90):  Serial.println("  Both Right..."); break;
+        case (0x60):  Serial.println("  Both Left..."); break;
+
+        default:      Serial.print("  What's this->");
+                      Serial.println(bumper,HEX);
+    }
+}
+
 
