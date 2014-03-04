c8fe57f71005bf9342ffa4c5dc6a7fb2d462631d
Lucas O'Conor <lvoconor@gmail.com>
2014-03-03 23:53:52 -0800

    Increased braking time and fixed compiler error for Beacon_Stop_Test

--- Beacon_Stop_Test/Beacon_Stop_Test.ino
+++ Beacon_Stop_Test/Beacon_Stop_Test.ino
@@ -16,6 +16,10 @@
 #define BEACON_INPUT_PIN 3
 #define BEACON_INTERRUPT_NUMBER 1
 #define TAPE_INTERRUPT_NUMBER 0  
+  
+#define SERVER_BEACON_MICROS 1176
+#define EXCHANGE_BEACON_MICROS 333
+#define NO_SIGNAL_MICROS 2000
 
 
 
@@ -26,6 +30,7 @@ volatile unsigned long previousTime = 0;
 volatile unsigned long period = NO_SIGNAL_MICROS; 
 volatile boolean risingEdgeFlag = false; 
 boolean beaconFoundFlag = false;
+int pause = 4;
 
 /*---------------- Arduino Main Functions -------------------*/
 void setup() {
@@ -33,7 +38,8 @@ void setup() {
   Serial.println("The Tape_Stop_Test program has started!");
   pinMode(TAPE_INPUT_PIN, INPUT);
   WinkleInit();  
-  SpinRight(8);
+  RightMtrSpeed(-4);
+  LeftMtrSpeed(4);
   TMRArd_InitTimer(0, 2);
   attachInterrupt(BEACON_INTERRUPT_NUMBER, rise_detected, RISING);
   Serial.println("Searching for beacon");
@@ -51,11 +57,15 @@ void loop() {
     TMRArd_InitTimer(0, 2);
 
   }
+  //If Beacon is found, then stop the rotation
   unsigned long currentPeriod = period;
   if(!beaconFoundFlag && currentPeriod < SERVER_BEACON_MICROS + 10 && currentPeriod > SERVER_BEACON_MICROS - 10 ){
     beaconFoundFlag = true;
-    SpinRight(0);
-    //DriveForward(2);
+      LeftMtrSpeed(-10);
+      RightMtrSpeed(10);
+      delayMicroseconds(12000);
+      LeftMtrSpeed(0); // motors not meant for instantaneous reverse so including a brief stop
+      RightMtrSpeed(0);
     Serial.println("850 hz beacon found - stopping ");
   }
   
