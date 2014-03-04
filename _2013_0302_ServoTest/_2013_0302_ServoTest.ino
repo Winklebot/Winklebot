a36edaff0d1361477120e3d84d839ecd82a3de9c
elizabethmarshman <elizabeth.marshman@gmail.com>
2014-03-03 17:21:46 -0800

    writing 90 or 0 for on or off

---
+++ _2013_0302_ServoTest/_2013_0302_ServoTest.ino
@@ -0,0 +1,55 @@
+//**************************************************************
+//  File:      ServoTest.pde 
+//  Contents:  This program operates a servo motor
+//
+//  Notes:    Target: Arduino UNO R1 & R2
+//            Arduino IDE version: 0022
+//
+//  History:
+//  when      who  what/why
+//  ----      ---  -------------------------------------------
+//  03/01/14  HMK  Program created
+//  03/02/14  ECM  Program updated with new pins
+//  
+//
+// control (side blue wire goes to pin (10)), black is ground, red is power
+//**************************************************************/
+
+/*---------------- Includes ---------------------------------*/
+//#include <SoftwareServo.h>
+//#include <Winklelib.h>
+#include <Servo.h>
+
+
+/*---------------- Module Defines ---------------------------*/
+Servo myservo;
+
+
+/*---------------- Module Function Prototypes ---------------*/
+
+/* HEAD */
+//unsigned char theKey;
+//unsigned char direct;
+int SERVO = 10;
+//int DIR1 = 12;
+//int ENABLE2 = 5;
+//int DIR2 = 4;
+//void RespToKey(void);
+
+
+/* FETCH_HEAD */
+/*---------------- Module Level Variables ---------------*/
+
+/*---------------- Arduino Main Functions -------------------*/
+void setup() { 
+  Serial.begin(9600);
+//  Serial.println("Starting ServoTest...");
+//  pinMode(SERVO, OUTPUT);
+  myservo.attach(10);
+}
+
+void loop() {  
+  myservo.write(0);
+}
+      
+/*---------------- Module Functions -------------------------*/
