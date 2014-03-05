03639418600d5e302383a8067d8a400e7a9c7571
hmkerrick <hmkerrick@gmail.com>
2014-03-05 01:39:43 -0800

    Drive Bumper test
    Not reading the bumper pins correctly… need to fix

--- MotorForwardButtonTest/MotorForwardButtonTest.ino
+++ MotorForwardButtonTest/MotorForwardButtonTest.ino
@@ -1,3 +1,4 @@
+
 /**************************************************************
   File:      MotorForwardButtonTest.pde 
   Contents:  This program drives the bot towards the server button
@@ -16,10 +17,11 @@
 #include <Winklelib.h>
 #include <Timers.h>
 #include <Servo.h>
-#define DRIVING_TOWARDS_EXCHANGE 1
-#define PRESSING 2
-#define DRIVING_TOWARDS_SERVER 3
-#define READY_TO_DUMP 4
+#define WAITING      1
+#define DRIVING_TOWARDS_EXCHANGE 2
+#define PRESSING 3
+#define DRIVING_TOWARDS_SERVER 4
+#define READY_TO_DUMP 5
 
 #define LEFT_FRONT_BUMPER        4
 #define RIGHT_FRONT_BUMPER       5
@@ -44,14 +46,15 @@ void setup() {
 }
 
 void loop() {
-
   switch(state) {
+//    case(WAITING) : 
+//      if
     case(DRIVING_TOWARDS_EXCHANGE) :
-      if(LEFT_FRONT_BUMPER && RIGHT_FRONT_BUMPER == 1) {  // both front bumpers are unhit
+      if(LEFT_FRONT_BUMPER == 1 && RIGHT_FRONT_BUMPER == 1) {  // both front bumpers are unhit
       Serial.println("Moving forward, no bumps");
       DriveForward(forwardspeed);
       }
-      if(LEFT_FRONT_BUMPER && RIGHT_FRONT_BUMPER == 0) {  // both front bumpers hit
+      if(LEFT_FRONT_BUMPER == 0 && RIGHT_FRONT_BUMPER == 0) {  // both front bumpers hit
       Serial.println("Aligned with exchange");
       DriveForward(0);
       ExchangeButtonCounter = ExchangeButtonCounter + 1;
@@ -82,7 +85,7 @@ void loop() {
       else DriveForward(-1 * forwardspeed); // backwards to the server
       state = DRIVING_TOWARDS_SERVER;
   case(DRIVING_TOWARDS_SERVER) :
-      if(LEFT_BACK_BUMPER && RIGHT_BACK_BUMPER == 0); {  // both front bumpers hit
+      if(LEFT_BACK_BUMPER == 0 && RIGHT_BACK_BUMPER == 0); {  // both front bumpers hit
       Serial.println("Aligned with server");
       DriveForward(0);
       state = READY_TO_DUMP;
@@ -93,5 +96,6 @@ void loop() {
 /*---------------- Module Functions -------------------------*/
 
 
+// My bumper hits are registered - coded wrong
 
 
